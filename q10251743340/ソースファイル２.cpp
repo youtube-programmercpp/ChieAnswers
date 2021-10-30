0804247084d80ba9300ff5ae87d0faa3c875d444
youtube-programmercpp <sbl908k@yahoo.co.jp>
2021-10-30 12:38:25 +0900

    インデント調整

--- q10251743340/ソースファイル２.cpp
+++ q10251743340/ソースファイル２.cpp
@@ -1,8 +1,8 @@
 #include"6-4.h"
 double AVE_MIN_MAX;
 double ave3(int a, int b, int c) {
-double ave;
-ave = (double)(a + b + c) / 3;
-AVE_MIN_MAX = (double)(MIN3 + MAX3) / 2;
-return ave;
+	double ave;
+	ave = (double)(a + b + c) / 3;
+	AVE_MIN_MAX = (double)(MIN3 + MAX3) / 2;
+	return ave;
 }
